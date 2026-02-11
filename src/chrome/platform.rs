93de6adfcbaa5783d49da534f3214278dc6215ec
Rich Nunley <rnunley@nunleymediagroup.com>
2026-02-10 23:51:05 -0600

    fix: pass channel by reference to linux and windows candidate functions

--- src/chrome/platform.rs
+++ src/chrome/platform.rs
@@ -92,12 +92,12 @@ fn chrome_candidates(channel: Channel) -> Vec<PathBuf> {
 
     #[cfg(target_os = "linux")]
     {
-        linux_candidates(channel)
+        linux_candidates(&channel)
     }
 
     #[cfg(target_os = "windows")]
     {
-        windows_candidates(channel)
+        windows_candidates(&channel)
     }
 
     #[cfg(not(any(target_os = "macos", target_os = "linux", target_os = "windows")))]
